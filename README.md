bb55cb7c919bb5d83f5f9e0f73b7e7d166439ef6
0xfoobar <0xfoobar1@gmail.com>
2022-08-06 15:08:39 -0400

    arbitrary data delegation

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # nft-delegation
+
+### Why NFT delegation?
+
+Proving ownership of an asset to a third party application in the Ethereum ecosystem is common. Users frequently sign payloads of data to authenticate themselves before gaining access to perform some operation. However, this method–akin to giving the third party root access to one’s main wallet–is both insecure and inconvenient.
+
+### Why not existing solutions?
+
+EIP712 signatures are not smart contract compatible. 
+ENS names are a dangerous & clunky dependency not suitable for an EIP standard.
+Some solutions are too specific or hardcoded for general reuse.
+
+Opinionated:
+- fully onchain, no EIP712 signatures
+- fully immutable, no admin powers
+- fully standalone, no external dependencies
+- fully identifiable, clear method names
+- reusable global registry w/ same address across multiple EVM chains
+
+Why?
+Onchain is critical for smart contract composability that can't produce a signature.
+Immutable is critical for any public good that will stand the test of time.
+Standalone is critical for ensuring the guarantees will stay valid.
+Identifiable is critical to avoid phishing scams when people are using vaults to interact with the registry.
+Reusable is critical so new projects can bootstrap off existing network effects.
+
+Use an ERC-165-esque hash list for specific permissions. Start out with claim permissions but can expand to more later.
+
+TODO: can we get onchain enumeration?
+TODO: can we get all-at-once revocation?
+TODO: can we get timelocked delegation for selling off airdrop rights?
+TODO: does the ens fuse wrapper match what we're doing here?
--- src/DelegationRegistry.sol
+++ src/DelegationRegistry.sol
@@ -31,51 +31,71 @@ contract DelegationRegistry {
 
     mapping(bytes32 => address) delegations;
 
-    event DelegateForAll(address _vault, address _delegate, bytes32 role);
-    event DelegateForCollection(address _vault, address _delegate, bytes32 role, address _collection);
-    event DelegateForToken(address _vault, address _delegate, bytes32 role, address _collection, uint256 _tokenId);
-
+    event DelegateForAll(address _vault, address _delegate, bytes32 _role);
+    event DelegateForCollection(address _vault, address _delegate, bytes32 _role, address _collection);
+    event DelegateForToken(address _vault, address _delegate, bytes32 _role, address _collection, uint256 _tokenId);
+    event DelegateFor(address _vault, address _delegate, bytes32 _role, bytes32 _data);
 
     ///////////
     // WRITE //
     ///////////
 
+    /// @notice Allow the delegate to act on your behalf for all NFT collections
     function delegateForAll(address _delegate, bytes32 _role) external {
         bytes32 delegateHash = keccak256(abi.encodePacked(_role, msg.sender));
         delegations[delegateHash] = _delegate;
         emit DelegateForAll(msg.sender, _delegate, _role);
     }
 
+    /// @notice Allow the delegate to act on your behalf for a specific NFT collection
     function delegateForCollection(address _delegate, bytes32 _role, address _collection) external {
         bytes32 delegateHash = keccak256(abi.encodePacked(_role, msg.sender, _collection));
         delegations[delegateHash] = _delegate;
         emit DelegateForCollection(msg.sender, _delegate, _role, _collection);
     }
 
+    /// @notice Allow the delegate to act on your behalf for a specific token, supports 721 and 1155
     function delegateForToken(address _delegate, bytes32 _role, address _collection, uint256 _tokenId) external {
         bytes32 delegateHash = keccak256(abi.encodePacked(_role, msg.sender, _collection, _tokenId));
         delegations[delegateHash] = _delegate;
         emit DelegateForToken(msg.sender, _delegate, _role, _collection, _tokenId);
     }
 
+    /// @notice A delegation generalization where the vault can pass arbitrary data to be interpreted
+    function delegateFor(address _delegate, bytes32 _role, bytes32 _data) external {
+        bytes32 delegateHash = keccak256(abi.encodePacked(_role, msg.sender, _data));
+        delegations[delegateHash] = _delegate;
+        emit DelegateFor(msg.sender, _delegate, _role, _data);
+    }
+
     //////////
     // READ //
     //////////
 
+    /// @notice Returns the address delegated to act on your behalf for all NFTs
     function getDelegateForAll(bytes32 _role, address _vault) public view returns (address) {
         bytes32 delegateHash = keccak256(abi.encodePacked(_role, _vault));
         return delegations[delegateHash];
     }
 
+    /// @notice Returns the address delegated to act on your behalf for an NFT collection
     function getDelegateForCollection(bytes32 _role, address _vault, address _collection) public view returns (address) {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_vault, _collection));
+        bytes32 delegateHash = keccak256(abi.encodePacked(_role, _vault, _collection));
         address delegate = delegations[delegateHash];
         return delegate != address(0x0) ? delegate : getDelegateForAll(_role, _vault);
     }
     
+    /// @notice Returns the address delegated to act on your behalf for an specific NFT
     function getDelegateForToken(bytes32 _role, address _vault, address _collection, uint256 _tokenId) public view returns (address) {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_vault, _collection, _tokenId));
+        bytes32 delegateHash = keccak256(abi.encodePacked(_role, _vault, _collection, _tokenId));
         address delegate = delegations[delegateHash];
         return delegate != address(0x0) ? delegate : getDelegateForCollection(_role, _vault, _collection);
+    }
+
+    /// @notice Returns the address delegated to act on your behalf for arbitrary data
+    function getDelegateFor(bytes32 _role, address _vault, bytes32 _data) public view returns (address) {
+        bytes32 delegateHash = keccak256(abi.encodePacked(_role, _vault, _data));
+        address delegate = delegations[delegateHash];
+        return delegate;
     }
 }
