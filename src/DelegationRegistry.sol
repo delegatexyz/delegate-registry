dff68861e2c1bd586addd598b1ddc480522330b1
0xfoobar <0xfoobar1@gmail.com>
2022-08-10 12:58:19 -0400

    Natspec docs

--- src/DelegationRegistry.sol
+++ src/DelegationRegistry.sol
@@ -9,54 +9,81 @@ contract DelegationRegistry {
     /// @notice The global mapping and single source of truth for delegations
     mapping(bytes32 => bool) delegations;
 
-    event DelegateForAll(address _vault, address _delegate, bytes32 _role, bool _value);
-    event DelegateForCollection(address _vault, address _delegate, bytes32 _role, address _collection, bool _value);
-    event DelegateForToken(address _vault, address _delegate, bytes32 _role, address _collection, uint256 _tokenId, bool _value);
+    /// @notice Emitted when a user delegates their entire wallet
+    event DelegateForAll(address vault, address delegate, bytes32 role, bool value);
+    /// @notice Emitted when a user delegates a specific collection
+    event DelegateForCollection(address vault, address delegate, bytes32 role, address collection, bool value);
+    /// @notice Emitted when a user delegates a specific token
+    event DelegateForToken(address vault, address delegate, bytes32 role, address collection, uint256 tokenId, bool value);
 
     ///////////
     // WRITE //
     ///////////
 
     /// @notice Allow the delegate to act on your behalf for all NFT collections
-    function delegateForAll(address _delegate, bytes32 _role, bool _value) external {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_delegate, _role, msg.sender));
-        delegations[delegateHash] = _value;
-        emit DelegateForAll(msg.sender, _delegate, _role, _value);
+    /// @param delegate The hotwallet to act on your behalf
+    /// @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
+    /// @param value Whether to enable or disable delegation for this address, true for setting and false for revoking
+    function delegateForAll(address delegate, bytes32 role, bool value) external {
+        bytes32 delegateHash = keccak256(abi.encodePacked(delegate, role, msg.sender));
+        delegations[delegateHash] = value;
+        emit DelegateForAll(msg.sender, delegate, role, value);
     }
 
     /// @notice Allow the delegate to act on your behalf for a specific NFT collection
-    function delegateForCollection(address _delegate, bytes32 _role, address _collection, bool _value) external {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_delegate, _role, msg.sender, _collection));
-        delegations[delegateHash] = _value;
-        emit DelegateForCollection(msg.sender, _delegate, _role, _collection, _value);
+    /// @param delegate The hotwallet to act on your behalf
+    /// @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
+    /// @param collection The contract address for the collection you're delegating
+    /// @param value Whether to enable or disable delegation for this address, true for setting and false for revoking
+    function delegateForCollection(address delegate, bytes32 role, address collection, bool value) external {
+        bytes32 delegateHash = keccak256(abi.encodePacked(delegate, role, msg.sender, collection));
+        delegations[delegateHash] = value;
+        emit DelegateForCollection(msg.sender, delegate, role, collection, value);
     }
 
     /// @notice Allow the delegate to act on your behalf for a specific token, supports 721 and 1155
-    function delegateForToken(address _delegate, bytes32 _role, address _collection, uint256 _tokenId, bool _value) external {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_delegate, _role, msg.sender, _collection, _tokenId));
-        delegations[delegateHash] = _value;
-        emit DelegateForToken(msg.sender, _delegate, _role, _collection, _tokenId, _value);
+    /// @param delegate The hotwallet to act on your behalf
+    /// @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
+    /// @param collection The contract address for the collection you're delegating
+    /// @param tokenId The token id for the token you're delegating
+    /// @param value Whether to enable or disable delegation for this address, true for setting and false for revoking
+    function delegateForToken(address delegate, bytes32 role, address collection, uint256 tokenId, bool value) external {
+        bytes32 delegateHash = keccak256(abi.encodePacked(delegate, role, msg.sender, collection, tokenId));
+        delegations[delegateHash] = value;
+        emit DelegateForToken(msg.sender, delegate, role, collection, tokenId, value);
     }
 
     //////////
     // READ //
     //////////
 
-    /// @notice Returns the address delegated to act on your behalf for all NFTs
-    function checkDelegateForAll(address _delegate, bytes32 _role, address _vault) public view returns (bool) {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_delegate, _role, _vault));
+    /// @notice Returns true if the address is delegated to act on your behalf for all NFTs
+    /// @param delegate The hotwallet to act on your behalf
+    /// @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
+    /// @param vault The cold wallet who issued the delegation
+    function checkDelegateForAll(address delegate, bytes32 role, address vault) public view returns (bool) {
+        bytes32 delegateHash = keccak256(abi.encodePacked(delegate, role, vault));
         return delegations[delegateHash];
     }
 
-    /// @notice Returns the address delegated to act on your behalf for an NFT collection
-    function checkDelegateForCollection(address _delegate, bytes32 _role, address _vault, address _collection) public view returns (bool) {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_role, _vault, _collection));
-        return delegations[delegateHash] ? true : checkDelegateForAll(_delegate, _role, _vault);
+    /// @notice Returns true if the address is delegated to act on your behalf for an NFT collection
+    /// @param delegate The hotwallet to act on your behalf
+    /// @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
+    /// @param collection The contract address for the collection you're delegating
+    /// @param vault The cold wallet who issued the delegation
+    function checkDelegateForCollection(address delegate, bytes32 role, address vault, address collection) public view returns (bool) {
+        bytes32 delegateHash = keccak256(abi.encodePacked(role, vault, collection));
+        return delegations[delegateHash] ? true : checkDelegateForAll(delegate, role, vault);
     }
     
-    /// @notice Returns the address delegated to act on your behalf for an specific NFT
-    function checkDelegateForToken(address _delegate, bytes32 _role, address _vault, address _collection, uint256 _tokenId) public view returns (bool) {
-        bytes32 delegateHash = keccak256(abi.encodePacked(_role, _vault, _collection, _tokenId));
-        return delegations[delegateHash] ? true : checkDelegateForCollection(_delegate, _role, _vault, _collection);
+    /// @notice Returns true if the address is delegated to act on your behalf for an specific NFT
+    /// @param delegate The hotwallet to act on your behalf
+    /// @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
+    /// @param collection The contract address for the collection you're delegating
+    /// @param tokenId The token id for the token you're delegating
+    /// @param vault The cold wallet who issued the delegation
+    function checkDelegateForToken(address delegate, bytes32 role, address vault, address collection, uint256 tokenId) public view returns (bool) {
+        bytes32 delegateHash = keccak256(abi.encodePacked(role, vault, collection, tokenId));
+        return delegations[delegateHash] ? true : checkDelegateForCollection(delegate, role, vault, collection);
     }
 }
