aea08d4051724dfe7ae6dbbec57eec66adc8e467
0xfoobar <0xfoobar1@gmail.com>
2022-08-12 12:46:07 -0400

    Remove roles

--- src/DelegationRegistry.sol
+++ src/DelegationRegistry.sol
@@ -18,60 +18,57 @@ contract DelegationRegistry {
     /** 
     * @notice Emitted when a user delegates their entire wallet
     */
-    event DelegateForAll(address vault, address delegate, bytes32 role, bool value);
+    event DelegateForAll(address vault, address delegate, bool value);
     
     /** 
     * @notice Emitted when a user delegates a specific collection
     */ 
-    event DelegateForCollection(address vault, address delegate, bytes32 role, address collection, bool value);
+    event DelegateForCollection(address vault, address delegate, address collection, bool value);
 
     /** 
     * @notice Emitted when a user delegates a specific token
     */
-    event DelegateForToken(address vault, address delegate, bytes32 role, address collection, uint256 tokenId, bool value);
+    event DelegateForToken(address vault, address delegate, address collection, uint256 tokenId, bool value);
 
     /** -----------  WRITE ----------- */
 
     /** 
     * @notice Allow the delegate to act on your behalf for all NFT collections
     * @param delegate The hotwallet to act on your behalf
-    * @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
     * @param value Whether to enable or disable delegation for this address, true for setting and false for revoking
     */
         
-    function delegateForAll(address delegate, bytes32 role, bool value) external {
-        bytes32 delegateHash = keccak256(abi.encode(delegate, role, msg.sender));
+    function delegateForAll(address delegate, bool value) external {
+        bytes32 delegateHash = keccak256(abi.encode(delegate, msg.sender));
         delegations[delegateHash] = value;
-        emit DelegateForAll(msg.sender, delegate, role, value);
+        emit DelegateForAll(msg.sender, delegate, value);
     }
 
     /** 
     * @notice Allow the delegate to act on your behalf for a specific NFT collection
     * @param delegate The hotwallet to act on your behalf
-    * @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
     * @param collection The contract address for the collection you're delegating
     * @param value Whether to enable or disable delegation for this address, true for setting and false for revoking
     */
 
-    function delegateForCollection(address delegate, bytes32 role, address collection, bool value) external {
-        bytes32 delegateHash = keccak256(abi.encode(delegate, role, msg.sender, collection));
+    function delegateForCollection(address delegate, address collection, bool value) external {
+        bytes32 delegateHash = keccak256(abi.encode(delegate, msg.sender, collection));
         delegations[delegateHash] = value;
-        emit DelegateForCollection(msg.sender, delegate, role, collection, value);
+        emit DelegateForCollection(msg.sender, delegate, collection, value);
     }
 
     /** 
     * @notice Allow the delegate to act on your behalf for a specific token, supports 721 and 1155
     * @param delegate The hotwallet to act on your behalf
-    * @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
     * @param collection The contract address for the collection you're delegating
     * @param tokenId The token id for the token you're delegating
     * @param value Whether to enable or disable delegation for this address, true for setting and false for revoking
     */    
 
-    function delegateForToken(address delegate, bytes32 role, address collection, uint256 tokenId, bool value) external {
-        bytes32 delegateHash = keccak256(abi.encode(delegate, role, msg.sender, collection, tokenId));
+    function delegateForToken(address delegate, address collection, uint256 tokenId, bool value) external {
+        bytes32 delegateHash = keccak256(abi.encode(delegate, msg.sender, collection, tokenId));
         delegations[delegateHash] = value;
-        emit DelegateForToken(msg.sender, delegate, role, collection, tokenId, value);
+        emit DelegateForToken(msg.sender, delegate, collection, tokenId, value);
     }
 
     /** -----------  READ ----------- */
@@ -79,39 +76,36 @@ contract DelegationRegistry {
     /** 
     * @notice Returns true if the address is delegated to act on your behalf for all NFTs
     * @param delegate The hotwallet to act on your behalf
-    * @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
     * @param vault The cold wallet who issued the delegation
     */
 
-    function checkDelegateForAll(address delegate, bytes32 role, address vault) public view returns (bool) {
-        bytes32 delegateHash = keccak256(abi.encode(delegate, role, vault));
+    function checkDelegateForAll(address delegate, address vault) public view returns (bool) {
+        bytes32 delegateHash = keccak256(abi.encode(delegate, vault));
         return delegations[delegateHash];
     }
 
     /** 
     * @notice Returns true if the address is delegated to act on your behalf for an NFT collection
     * @param delegate The hotwallet to act on your behalf
-    * @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
     * @param collection The contract address for the collection you're delegating
     * @param vault The cold wallet who issued the delegation
     */
         
-    function checkDelegateForCollection(address delegate, bytes32 role, address vault, address collection) public view returns (bool) {
-        bytes32 delegateHash = keccak256(abi.encode(delegate, role, vault, collection));
-        return delegations[delegateHash] ? true : checkDelegateForAll(delegate, role, vault);
+    function checkDelegateForCollection(address delegate, address vault, address collection) public view returns (bool) {
+        bytes32 delegateHash = keccak256(abi.encode(delegate, vault, collection));
+        return delegations[delegateHash] ? true : checkDelegateForAll(delegate, vault);
     }
     
     /** 
     * @notice Returns true if the address is delegated to act on your behalf for an specific NFT
     * @param delegate The hotwallet to act on your behalf
-    * @param role The role for delegations, default is 0x0000000000000000000000000000000000000000000000000000000000000000
     * @param collection The contract address for the collection you're delegating
     * @param tokenId The token id for the token you're delegating
     * @param vault The cold wallet who issued the delegation
     */
 
-    function checkDelegateForToken(address delegate, bytes32 role, address vault, address collection, uint256 tokenId) public view returns (bool) {
-        bytes32 delegateHash = keccak256(abi.encode(delegate, role, vault, collection, tokenId));
-        return delegations[delegateHash] ? true : checkDelegateForCollection(delegate, role, vault, collection);
+    function checkDelegateForToken(address delegate, address vault, address collection, uint256 tokenId) public view returns (bool) {
+        bytes32 delegateHash = keccak256(abi.encode(delegate, vault, collection, tokenId));
+        return delegations[delegateHash] ? true : checkDelegateForCollection(delegate, vault, collection);
     }
 }
--- test/DelegationRegistry.t.sol
+++ test/DelegationRegistry.t.sol
@@ -12,36 +12,36 @@ contract DelegationRegistryTest is Test {
         reg = new DelegationRegistry();
     }
 
-    function testApproveAndRevokeForAll(address vault, address delegate, bytes32 role) public {
+    function testApproveAndRevokeForAll(address vault, address delegate) public {
         // Approve
         vm.startPrank(vault);
-        reg.delegateForAll(delegate, role, true);
-        assertTrue(reg.checkDelegateForAll(delegate, role, vault));
-        assertTrue(reg.checkDelegateForCollection(delegate, role, vault, address(0x0)));
-        assertTrue(reg.checkDelegateForToken(delegate, role, vault, address(0x0), 0));
+        reg.delegateForAll(delegate, true);
+        assertTrue(reg.checkDelegateForAll(delegate, vault));
+        assertTrue(reg.checkDelegateForCollection(delegate, vault, address(0x0)));
+        assertTrue(reg.checkDelegateForToken(delegate, vault, address(0x0), 0));
         // Revoke
-        reg.delegateForAll(delegate, role, false);
-        assertFalse(reg.checkDelegateForAll(delegate, role, vault));
+        reg.delegateForAll(delegate, false);
+        assertFalse(reg.checkDelegateForAll(delegate, vault));
     }
 
-    function testApproveAndRevokeForCollection(address vault, address delegate, bytes32 role, address collection) public {
+    function testApproveAndRevokeForCollection(address vault, address delegate, address collection) public {
         // Approve
         vm.startPrank(vault);
-        reg.delegateForCollection(delegate, role, collection, true);
-        assertTrue(reg.checkDelegateForCollection(delegate, role, vault, collection));
-        assertTrue(reg.checkDelegateForToken(delegate, role, vault, collection, 0));
+        reg.delegateForCollection(delegate, collection, true);
+        assertTrue(reg.checkDelegateForCollection(delegate, vault, collection));
+        assertTrue(reg.checkDelegateForToken(delegate, vault, collection, 0));
         // Revoke
-        reg.delegateForCollection(delegate, role, collection, false);
-        assertFalse(reg.checkDelegateForCollection(delegate, role, vault, collection));
+        reg.delegateForCollection(delegate, collection, false);
+        assertFalse(reg.checkDelegateForCollection(delegate, vault, collection));
     }
 
-    function testApproveAndRevokeForToken(address vault, address delegate, bytes32 role, address collection, uint256 tokenId) public {
+    function testApproveAndRevokeForToken(address vault, address delegate, address collection, uint256 tokenId) public {
         // Approve
         vm.startPrank(vault);
-        reg.delegateForToken(delegate, role, collection, tokenId, true);
-        assertTrue(reg.checkDelegateForToken(delegate, role, vault, collection, tokenId));
+        reg.delegateForToken(delegate, collection, tokenId, true);
+        assertTrue(reg.checkDelegateForToken(delegate, vault, collection, tokenId));
         // Revoke
-        reg.delegateForToken(delegate, role, collection, tokenId, false);
-        assertFalse(reg.checkDelegateForToken(delegate, role, vault, collection, tokenId));
+        reg.delegateForToken(delegate, collection, tokenId, false);
+        assertFalse(reg.checkDelegateForToken(delegate, vault, collection, tokenId));
     }
 }
