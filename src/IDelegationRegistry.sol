4b66597f4ed7241564fe0a2fd909e27e2ef4bf29
Michael Reynolds <103044686+mireynolds@users.noreply.github.com>
2023-05-04 18:17:03 +0100

    Pushing some comments for reference

--- src/IDelegationRegistry.sol
+++ src/IDelegationRegistry.sol
@@ -37,10 +37,13 @@ interface IDelegationRegistry {
     event DelegateForContract(address indexed vault, address indexed delegate, address indexed contract_, bool value, bytes32 data);
 
     /// @notice Emitted when a user delegates a specific token
-    event DelegateForToken(address indexed vault, address indexed delegate, address indexed contract_, uint256 number, bool value, bytes32 data);
+    event DelegateForToken(address indexed vault, address indexed delegate, address indexed contract_, uint256 tokenId, bool value, bytes32 data);
 
     /// @notice Emitted when a user delegates a fungible balance
     event DelegateForBalance(address indexed vault, address indexed delegate, address indexed contract_, uint256 balance, bool value, bytes32 data);
+
+    /// @notice Emitted when a user delegates a specific token with a specific balance
+    event DelegateForTokenBalance(address indexed vault, address indexed delegate, address indexed contract_, uint256 tokenId, uint256 balance, bool value, bytes32 data);
 
     /**
      * -----------  WRITE -----------
@@ -86,6 +89,7 @@ interface IDelegationRegistry {
      */
     function delegateForBalance(address delegate, address contract_, uint256 balance, bool value, bytes32 data) external;
 
+
     /**
      * -----------  READ -----------
      */
@@ -129,7 +133,8 @@ interface IDelegationRegistry {
     function checkDelegateForToken(address delegate, address vault, address contract_, uint256 tokenId, bytes32 data) external view returns (bool);
 
     /**
-     * @notice Returns true if the address is delegated to act on the behalf of a fungible balance, the token's contract, or an entire vault
+     * @notice Returns true if the address is delegated to act on the behalf of a specific fungible balance, the token's contract, or an entire vault
+     * @dev we may need to change this method or create another method since balance is an upper bound
      * @param delegate The hotwallet to act on your behalf
      * @param contract_ The address for the contract you're delegating
      * @param balance The fungible balance being delegated
