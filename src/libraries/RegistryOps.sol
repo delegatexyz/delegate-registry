3562ecdfd1cb642090afa3f03e3180550be79a07
Vectorized <webby1111@hotmail.com>
2023-08-09 08:26:00 +0000

    Add comments for max function

--- src/libraries/RegistryOps.sol
+++ src/libraries/RegistryOps.sol
@@ -5,6 +5,12 @@ library RegistryOps {
     /// @dev `x > y ? x : y`.
     function max(uint256 x, uint256 y) internal pure returns (uint256 z) {
         assembly {
+            // `gt(y, x)` will evaluate to 1 if `y > x`, else 0.
+            //
+            // If `y > x`:
+            //     `x ^ ((x ^ y) * 1) = x ^ (x ^ y) = (x ^ x) ^ y = 0 ^ y = y`.
+            // otherwise:
+            //     `x ^ ((x ^ y) * 0) = x ^ 0 = x`.
             z := xor(x, mul(xor(x, y), gt(y, x)))
         }
     }
