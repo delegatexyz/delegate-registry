12e4dfa4af3a3f01fdf55e1725e5c157f8162500
0xfoobar <0xfoobar1@gmail.com>
2023-08-14 09:15:16 -0400

    use revert strings for broad version and chain explorer compat

--- src/singlesig/Singlesig.sol
+++ src/singlesig/Singlesig.sol
@@ -1,35 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.4;
 
+/// @dev This does not include receiver callbacks for "safe" transfer methods of ERC721, ERC1155, etc
 contract Singlesig {
     address public owner;
     address public pendingOwner;
-
-    /// @dev The caller account is not authorized to perform an operation
-    error OwnableUnauthorizedAccount(address account);
 
     event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     /// @dev Initializes the contract setting the address provided by the deployer as the initial owner
+    /// @dev Distinct constructor args will lead to distinct CREATE2 deployment bytecode, so no collision risk here
     constructor(address initialOwner) {
-        // TODO: Does a constructor arg affect the CREATE2 address?
         owner = initialOwner;
         emit OwnershipTransferred(address(0), initialOwner);
     }
 
-    // TODO: Do we need to implement receiver functions to get 1155s and others?
-
-    // Function to receive Ether. msg.data must be empty
+    // @dev Function to receive Ether. msg.data must be empty
     receive() external payable {}
 
-    // Fallback function is called when msg.data is not empty
+    // @dev Fallback function is called when msg.data is not empty
     fallback() external payable {}
 
     /// @dev Throws if called by any account other than the owner
     modifier onlyOwner() {
-        if (owner != msg.sender) revert OwnableUnauthorizedAccount(msg.sender);
+        require(owner == msg.sender, "Ownable2Step: caller is not the owner");
         _;
     }
 
