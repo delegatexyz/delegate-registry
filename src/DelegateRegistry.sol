fa0d191bed4fcbe7b84184cbe84139d65f7bef71
0xfoobar <0xfoobar1@gmail.com>
2023-08-25 15:57:15 -0400

    add test

--- src/DelegateRegistry.sol
+++ src/DelegateRegistry.sol
@@ -136,8 +136,8 @@ contract DelegateRegistry is IDelegateRegistry {
     function sweep() external {
         // TODO: Replace this with correct address
         // This hardcoded address is a CREATE2 factory counterfactual smart contract wallet that will always accept native token transfers
-        //slither-disable-next-line unchecked-lowlevel
-        0x0000000000000000000000000000000000000000.call{value: address(this).balance}("");
+        uint256 sc = uint256(uint160(0x0000000000000000000000000000000000000000)); 
+        assembly ("memory-safe") { let result := call(gas(), sc, selfbalance(), 0, 0, 0, 0) }
     }
 
     /**
