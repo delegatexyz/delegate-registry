3093d6bb3c1979f59842c70e62f0a22a5d3e7542
0xfoobar <0xfoobar1@gmail.com>
2023-09-21 09:40:10 -0400

    deployed delegateregistry to 0x00000000000000447e69651d841bD8D104Bed493

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -14,18 +14,23 @@ interface ImmutableCreate2Factory {
 
 contract Deploy is Script {
     ImmutableCreate2Factory immutable factory = ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
-    // bytes initCode = type(DelegateRegistry).creationCode;
-    // bytes32 salt = 0x00000000000000000000000000000000000000008b99e5a778edb02572010000;
+    bytes initCode = type(DelegateRegistry).creationCode;
+    // bytes32 salt = 0x0000000000000000000000000000000000000000fbe49ecfc3decb1164228b89;
+    bytes32 salt = 0x00000000000000000000000000000000000000002bbc593dd77cb93fbb932d5f;
 
-    bytes initCode = abi.encodePacked(type(Singlesig).creationCode, abi.encode(address(0x6Ed7D526b020780f694f3c10Dfb25E1b134D3215)));
-    bytes32 salt = 0x000000000000000000000000000000000000000016c7768a8c7a2824b846321d;
+    // bytes initCode = abi.encodePacked(type(Singlesig).creationCode, abi.encode(address(0x6Ed7D526b020780f694f3c10Dfb25E1b134D3215)));
+    // bytes32 salt = 0x000000000000000000000000000000000000000016c7768a8c7a2824b846321d;
 
     function run() external {
         vm.startBroadcast();
 
-        address singlesigAddress = factory.safeCreate2(salt, initCode);
-        Singlesig singlesig = Singlesig(payable(singlesigAddress));
-        console2.log(address(singlesig));
+        // address singlesigAddress = factory.safeCreate2(salt, initCode);
+        // Singlesig singlesig = Singlesig(payable(singlesigAddress));
+        // console2.log(address(singlesig));
+
+        address registryAddress = factory.safeCreate2(salt, initCode);
+        DelegateRegistry registry = DelegateRegistry(registryAddress);
+        console2.log(address(registry));
 
         // address registryAddress = factory.safeCreate2(salt, initCode);
         // DelegateRegistry registry = DelegateRegistry(registryAddress);
--- test/InitCodeHash.t.sol
+++ test/InitCodeHash.t.sol
@@ -6,21 +6,11 @@ import {console2} from "forge-std/console2.sol";
 import {DelegateRegistry} from "src/DelegateRegistry.sol";
 import {Singlesig} from "src/singlesig/Singlesig.sol";
 
-// Singlesig(foobardev) = 0x126583f6c09a06513f110bf1754633061630875211d1322b6e86c984481a003a
-// Seed 0x0000000000000000000000000000000000000000010611e6a73e9c03d5b15d36 => 0x0000000081117a317387DA0AD2873821cb7E40f8
-
-// With constructor params appended: 0x52d978fa1df058a3037a1db29546caf0c19bc9c602110fa48f6ccf39f5541bb0
-// Per https://twitter.com/0mnus/status/1691193474228641792
-
-// Delegate address: 0x6ed7d526b020780f694f3c10dfb25e1b134d3215
-// inithash: 0xcca90965f49f0b18661a05a21aea51ef95c0d27e2c3f216e54c928968e778af2
-// Seed 0x000000000000000000000000000000000000000023a8e79523c02100bd88400e => 0x00000000c1b78A4F3171fc53bD6A3CA05FceD326
-// We want to start with 0x000000de1e80 which is 6 bytes
-// Seed 0x00000000000000000000000000000000000000005d7cde2e4e4a52175e200216 => 0x000004De1E80EFb94a4383fFE3418f916027A251
-// Seed 0x0000000000000000000000000000000000000000eab62839b3422a092c19bdab => 0x000000de1E803040Fba6B848D410a55FaB8B3256
-
-// new inithash: 0xf911e320d18e7274491e7ab207bfff830e2926248f86c6a987668e8e72e1ed77
+// Singlesig inithash: 0xf911e320d18e7274491e7ab207bfff830e2926248f86c6a987668e8e72e1ed77
 // salt 0x000000000000000000000000000000000000000016c7768a8c7a2824b846321d => 0x000000dE1E80ea5a234FB5488fee2584251BC7e8
+
+// Registry inithash: 0x78bdba7d5e0c91d9aedc93c97bf84433daaf008a83bfe921c2d27ab77301d6d9
+// salt 0x0000000000000000000000000000000000000000fbe49ecfc3decb1164228b89 => 0x0000000000006DE22EeA995bE2f0511186b8e013 => 16777216
 
 contract InitCodeHashTest is Test {
     DelegateRegistry reg;
@@ -32,8 +22,8 @@ contract InitCodeHashTest is Test {
     }
 
     function getInitHash() public pure returns (bytes32) {
-        // bytes memory bytecode = type(DelegateRegistry).creationCode;
-        bytes memory initCode = abi.encodePacked(type(Singlesig).creationCode, abi.encode(address(0x6Ed7D526b020780f694f3c10Dfb25E1b134D3215)));
+        bytes memory initCode = type(DelegateRegistry).creationCode;
+        // bytes memory initCode = abi.encodePacked(type(Singlesig).creationCode, abi.encode(address(0x6Ed7D526b020780f694f3c10Dfb25E1b134D3215)));
         // console2.logBytes(initCode);
 
         return keccak256(abi.encodePacked(initCode));
