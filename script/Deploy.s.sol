c90f811d8abb4191aaedc7760ab8eddee9ff6cd6
0xfoobar <0xfoobar1@gmail.com>
2025-01-16 17:12:20 -0500

    E2E deployment to 0x0000000059A24EB229eED07Ac44229DB56C5d797

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -28,7 +28,6 @@ interface ZksyncCreate2Factory {
 contract Deploy is Script {
     address immutable firstOwner = 0x6Ed7D526b020780f694f3c10Dfb25E1b134D3215;
 
-    // ZksyncCreate2Factory immutable zksyncCreateFactory = ZksyncCreate2Factory(0x0000000000000000000000000000000000010000);
     Create2Factory immutable zksyncCreateFactory = Create2Factory(0x0000000000000000000000000000000000010000);
     ZksyncCreate2Factory immutable zksyncContractDeployer = ZksyncCreate2Factory(0x0000000000000000000000000000000000008006);
 
@@ -38,8 +37,7 @@ contract Deploy is Script {
     // bytes32 registrySalt = 0x00000000000000000000000000000000000000002bbc593dd77cb93fbb932d5f;
 
     bytes32 zkSinglesigSalt = 0x000000000000000000000000000000000000000091430a7ea982667861000090;
-    // bytes32 registrySalt = bytes32(0);
-    bytes32 registrySalt = 0x0000000000000000000000000000000000000000dc73d3e78582b023010000a0;
+    bytes32 zkRegistrySalt = 0x000000000000000000000000000000000000000077151a04325c7971a80c00a0;
 
     // bytes initCode = abi.encodePacked(type(Singlesig).creationCode, abi.encode(address(0x6Ed7D526b020780f694f3c10Dfb25E1b134D3215)));
     // bytes32 salt = 0x000000000000000000000000000000000000000016c7768a8c7a2824b846321d;
@@ -47,46 +45,43 @@ contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
 
-        Singlesig predeploySinglesig = new Singlesig{salt: zkSinglesigSalt}(firstOwner);
-        bytes32 singlesigZKBytecodeHash = ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT.getRawCodeHash(address(predeploySinglesig));
-        bytes32 constructorHash = keccak256(abi.encode(firstOwner));
-        console2.log(address(predeploySinglesig));
-        console2.logBytes(abi.encode(firstOwner));
-        console2.logBytes32(singlesigZKBytecodeHash);
-        console2.logBytes32(constructorHash);
-        bytes memory preimage = bytes.concat(CREATE2_PREFIX, bytes32(uint256(uint160(address(zksyncCreateFactory)))), zkSinglesigSalt, singlesigZKBytecodeHash, constructorHash);
-        console2.logBytes(preimage);
+        // Singlesig predeploySinglesig = new Singlesig{salt: zkSinglesigSalt}(firstOwner);
+        // bytes32 singlesigZKBytecodeHash = ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT.getRawCodeHash(address(predeploySinglesig));
+        // bytes32 constructorHash = keccak256(abi.encode(firstOwner));
+        // console2.log(address(predeploySinglesig));
+        // console2.logBytes(abi.encode(firstOwner));
+        // console2.logBytes32(singlesigZKBytecodeHash);
+        // console2.logBytes32(constructorHash);
+        // bytes memory preimage = bytes.concat(CREATE2_PREFIX, bytes32(uint256(uint160(address(zksyncCreateFactory)))), zkSinglesigSalt, singlesigZKBytecodeHash, constructorHash);
+        // console2.logBytes(preimage);
 
-        // address singlesigAddress = factory.safeCreate2(singlesigSalt, initCode);
-        // Singlesig singlesig = Singlesig(payable(singlesigAddress));
-        // console2.log(address(singlesig));
+        // --------
 
         // DelegateRegistry existing = DelegateRegistry(0x6b176c958fb89Ddca0fc8214150DA4c4D0a32fbe);
         // bytes32[] memory hashes = existing.getOutgoingDelegationHashes(0x86362a4C99d900D72d787Ef1BddA38Fd318aa5E9);
         // console2.logBytes32(hashes[0]);
 
-        // DelegateRegistry saltDeploy = new DelegateRegistry{salt: registrySalt}();
-        // console2.logBytes32(keccak256(""));
-        // console2.log(address(saltDeploy));
+        DelegateRegistry saltDeploy = new DelegateRegistry{salt: zkRegistrySalt}();
+        console2.logBytes32(keccak256(""));
+        console2.log(address(saltDeploy));
 
         // DelegateRegistry predeploy = new DelegateRegistry();
 
         // bytes32 bytecodeHash = keccak256(initCode);
-        // bytes32 zkBytecodeHash = ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT.getRawCodeHash(address(predeploy));
+        bytes32 zkBytecodeHash = ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT.getRawCodeHash(address(saltDeploy));
 
         // console2.logBytes32(bytecodeHash);
-        // console2.logBytes32(zkBytecodeHash);
-        // console2.log(address(predeploy));
-        // console2.log(msg.sender);
-        // console2.logBytes32(registrySalt);
-        // bytes memory preimage = bytes.concat(CREATE2_PREFIX, bytes32(uint256(uint160(address(zksyncCreateFactory)))), registrySalt, zkBytecodeHash, keccak256(""));
-        // console2.logBytes(preimage);
-        // address localRegistryAddress = address(uint160(uint256(keccak256(
-        //     bytes.concat(CREATE2_PREFIX, bytes32(uint256(uint160(address(zksyncCreateFactory)))), registrySalt, zkBytecodeHash, keccak256(""))
-        // ))));
-        // console2.log(localRegistryAddress);
-        // address registryAddress = zksyncContractDeployer.getNewAddressCreate2(address(zksyncCreateFactory), zkBytecodeHash, registrySalt, "");
-        // console2.log(registryAddress);
+        console2.logBytes32(zkBytecodeHash);
+        console2.log(msg.sender);
+        console2.logBytes32(zkRegistrySalt);
+        bytes memory preimage = bytes.concat(CREATE2_PREFIX, bytes32(uint256(uint160(address(zksyncCreateFactory)))), zkRegistrySalt, zkBytecodeHash, keccak256(""));
+        console2.logBytes(preimage);
+        address localRegistryAddress = address(uint160(uint256(keccak256(
+            bytes.concat(CREATE2_PREFIX, bytes32(uint256(uint160(address(zksyncCreateFactory)))), zkRegistrySalt, zkBytecodeHash, keccak256(""))
+        ))));
+        console2.log(localRegistryAddress);
+        address registryAddress = zksyncContractDeployer.getNewAddressCreate2(address(zksyncCreateFactory), zkBytecodeHash, zkRegistrySalt, "");
+        console2.log(registryAddress);
 
         // zksyncCreateFactory.create2(registrySalt, zkBytecodeHash, "");
 
